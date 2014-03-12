53daa2c48573b5ca2797bdadcb2602594288bf85
mihaicostin <mihai.costin@gmail.com>
2014-03-12 14:11:52 +0200

    Spring data over JPA with Hibernate as provider. The DAO is reduced to an interface.

---
+++ java-persistence/spring-data-hibernate/src/main/java/com/mc/spring/samples/console/persistence/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.mc.spring.samples.console.persistence.dao;
+
+import com.mc.spring.samples.console.persistence.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * This DAO is using the hibernate session directly.
+ * It's marked with @Repository for the exception handling - see PersistenceExceptionTranslationPostProcessor.
+ */
+@Repository
+public interface UserDao extends CrudRepository<User, Long> {
+
+    User findByUserName(String username);
+
+}
---
+++ java-persistence/spring-data-hibernate/src/main/java/com/mc/spring/samples/console/service/UserService.java
@@ -0,0 +1,36 @@
+package com.mc.spring.samples.console.service;
+
+import com.mc.spring.samples.console.persistence.dao.UserDao;
+import com.mc.spring.samples.console.persistence.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional
+    public void saveUser(User user) {
+        userDao.save(user);
+    }
+
+    @Transactional
+    public List<User> getUsers() {
+        Iterator<User> iterator = userDao.findAll().iterator();
+        ArrayList<User> users = new ArrayList<>();
+        while (iterator.hasNext()) {
+            users.add(iterator.next());
+        }
+        return users;
+    }
+
+
+}
