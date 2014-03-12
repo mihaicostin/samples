c3ca88a7ef4eba2f07ae3c764b5c0997e730e5b8
mihaicostin <mihai.costin@gmail.com>
2014-03-12 14:27:44 +0200

    update javadoc

--- java-persistence/spring-data-hibernate/src/main/java/com/mc/spring/samples/console/persistence/dao/UserDao.java
+++ java-persistence/spring-data-hibernate/src/main/java/com/mc/spring/samples/console/persistence/dao/UserDao.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Repository;
 
 
 /**
- * This DAO is using the hibernate session directly.
+ * This DAO is just an interface extending the provided spring-data repository.
+ * </br>
+ * New methods will need to respect the conventions defined by spring-data:
+ * http://docs.spring.io/spring-data/jpa/docs/current/reference/html/jpa.repositories.html
+ * in order to be transformed into valid queries.
+ *
  * It's marked with @Repository for the exception handling - see PersistenceExceptionTranslationPostProcessor.
  */
 @Repository
